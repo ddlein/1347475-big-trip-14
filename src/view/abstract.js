fa9190049dfe549f78454b70c3772e3ff94072e7
Дима <70834132+ddlein@users.noreply.github.com>
2021-05-22 15:47:42 +0300

    правки

--- src/view/abstract.js
+++ src/view/abstract.js
@@ -20,7 +20,6 @@ export default class Abstract {
     if (!this._element) {
       this._element = createElement(this.getTemplate());
     }
-    // this._element = !this._element ? createElement(this.getTemplate()) : this._element;
     return this._element;
   }
 
